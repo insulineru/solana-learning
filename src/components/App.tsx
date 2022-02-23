41a97ac3778b3a526ac8a9f7103b7620c01461bc
Aleksey Lisun <insuline.ru@gmail.com>
2022-02-23 21:48:51 +0300

    ✨ Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen text-white bg-gray-900">
+      <h1 className="text-6xl font-bold">
+        🎶 Blockchain russian rap leaderboard
+      </h1>
+      <h2 className="my-4 text-2xl">
+        View your favorite albums in metaverse ✨
+      </h2>
+    </div>
+  )
+}
+
+export default App
